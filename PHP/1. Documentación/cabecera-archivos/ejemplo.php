68e494fb91a4694960060efc25ad5a9e553a2176
Adv-richard <richard@adventures.do>
2015-02-09 15:06:32 -0400

    correciones del anterior push

--- PHP/1. Documentación/cabecera-archivos/ejemplo.php
+++ PHP/1. Documentación/cabecera-archivos/ejemplo.php
@@ -4,12 +4,10 @@
 *
 * Este archivo ejemplifica cómo debe estar estructurado un archivo y su documentación inicial . (termina en punto)
 *
-* @link http://github.com/richardblondet/AdventuresDigitalAgencyStandards/
+* @link https://github.com/richardblondet/AdventuresDigitalAgencyStandards/blob/master/PHP/1.%20Documentaci%C3%B3n/cabecera-archivos/ejemplo.php
 * @since DESCONOCIDA(valor por default: DESCONOCIDA)
 *
 * @package AdventuresDigitalAgencyStandards
 * @subpackage Documentación_cabecera-archivos
 */
-
-
 ?>
--- PHP/1. Documentación/funciones-clases/README.md
+++ PHP/1. Documentación/funciones-clases/README.md
@@ -1,5 +1,7 @@
 Documentación
 ===
+Funciones y Clases
+---
 La idea de aplicar estas prácticas es para luego implementar una herramienta que permita generar la documentación automáticamente a partir de lo que contiene el código fuente, una de estas herramientas en evaluación es [phpDocumentator](http://phpdoc.org/). 
 
 Use espacios dentro del bloque, no fichas, y asegurarse de que los elementos de cada grupo de variables están alineadas según los ejemplos.
